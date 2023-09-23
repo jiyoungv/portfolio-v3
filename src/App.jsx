54a5c254610c5c3fd19e2f9d171598339e9a35c2
지영 <jiyoungvvvvv@gmail.com>
2023-09-23 16:47:03 +0900

    Save: 작업중 - 구조 잡기(40%)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import { RiLink, RiFlaskFill, RiGithubFill } from 'react-icons/ri';
+// import { RiLink, RiFlaskFill, RiGithubFill } from 'react-icons/ri';
+import parse from 'html-react-parser';
 import AppLayout from 'component/AppLayout';
-import temp_img from 'asset/image/temp_img.png';
+import { profileInfoList } from 'util/data';
+// import temp_img from 'asset/image/temp_img.png';
 
 const App = () => {
   return (
@@ -15,7 +17,7 @@ const App = () => {
             </div>
           </div>
           <div className="table-row">
-            <div className="table-col-2">
+            <div className="table-col-4">
               <article className="main-logo">
                 <h1>P.</h1>
               </article>
@@ -25,13 +27,91 @@ const App = () => {
                 <h6>Ji Young <br/>Park</h6>
               </article>
             </div>
-            <div className="table-col-18">
-              2
+            <div className="table-col-14">
+              <article className="main-profile-info">
+                {profileInfoList.map((v, i) => (
+                  <dl key={i}>
+                    <dt>{v.title}</dt>
+                    <dd>{parse(v.text)}</dd>
+                  </dl>
+                ))}
+              </article>
             </div>
           </div>
           <div className="table-row">
             <div className="table-col">
-              2
+              <article className="main-career-collect">
+                <figure>
+                  TODO: 코딩 사진 or 했던 프로젝트, 클라이언트 로고(바둑판)
+                </figure>
+              </article>
+            </div>
+          </div>
+          <div className="main-career-head">
+            <div className="table-row">
+              <div className="table-col">
+                <article className="main-career-title">
+                  <h6>career</h6>
+                </article>
+              </div>
+            </div>
+            <div className="table-row">
+              <div className="table-col-12">
+                <article className="main-career-head-col">
+                  <p>company</p>
+                </article>
+              </div>
+              <div className="table-col-12">
+                <article className="main-career-head-col">
+                  <p>project</p>
+                </article>
+              </div>
+            </div>
+            <div className="table-row">
+              <div className="table-col-4">
+                <article className="main-career-head-col">
+                  <p>name</p>
+                </article>
+              </div>
+              <div className="table-col-4">
+                <article className="main-career-head-col">
+                  <p>period</p>
+                </article>
+              </div>
+              <div className="table-col-4">
+                <article className="main-career-head-col">
+                  <p>description</p>
+                </article>
+              </div>
+              <div className="table-col-4">
+                <article className="main-career-head-col">
+                  <p>device / name / link</p>
+                </article>
+              </div>
+              <div className="table-col-4">
+                <article className="main-career-head-col">
+                  <p>period</p>
+                </article>
+              </div>
+              <div className="table-col-4">
+                <article className="main-career-head-col">
+                  <p>description</p>
+                </article>
+              </div>
+            </div>
+          </div>
+          <div className="table-row">
+            <div className="table-col">
+              <article style={{ height: 800 }}>
+                내용
+              </article>
+            </div>
+          </div>
+          <div className="table-row">
+            <div className="table-col">
+              <article>
+                d
+              </article>
             </div>
           </div>
           <div className="table-row">
---
+++ src/util/data.js
@@ -0,0 +1,26 @@
+export const profileInfoList = [
+  {
+    title: `location`,
+    text: `Seoul, Korea`,
+  },
+  {
+    title: `email`,
+    text: `<a href="mailto:jiyoungvvvvv@gmail.com" title="Send to Email">jiyoungvvvvv@gmail.com</a>`,
+  },
+  {
+    title: `github`,
+    text: `<a href="https://github.com/jiyoungv" target="_blank" rel="noreferrer">https://github.com/jiyoungv</a>`,
+  },
+  {
+    title: `linkedin`,
+    text: `https://www.linkedin.com/in/jyp-it/`,
+  },
+  {
+    title: `skill`,
+    text: `HTML, CSS, SASS(SCSS), JavaScript(ES6+), jQuery, React, CSS-in-JS, Redux, Recoil, Storybook, GIT`,
+  },
+  {
+    title: `experience`,
+    text: `JSP, PHP, Node,js, Figma, Zeplin`,
+  },
+];
